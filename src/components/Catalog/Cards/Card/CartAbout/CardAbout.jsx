d7bca67de1257705824f325dfeaed980ea0b5dd5
kaisyQ <rutyo8@mail.ru>
2023-03-16 06:26:58 +0300

    card-slider

--- src/components/Catalog/Cards/Card/CartAbout/CardAbout.jsx
+++ src/components/Catalog/Cards/Card/CartAbout/CardAbout.jsx
@@ -1,17 +1,19 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 
+import * as Icon from 'react-bootstrap-icons'
+
 import React from "react"
 import styled from "styled-components"
 
 
-import { Carousel } from 'react-responsive-carousel'
 import Button from "../../../../Custom/Button"
 import { useParams } from "react-router-dom"
+
 
 const CardAboutWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
     text-align: left;
     gap: 1.5rem;
     z-index: 1;
@@ -56,6 +58,64 @@ const Text = styled.p`
     text-align: justify;
 `
 
+
+const SliderWrapper = styled.div`
+    
+`
+
+const Slider = styled.div`
+    position: relative;
+    width: 35rem;
+    height: 35rem;
+    background-color: #b19595;
+    overflow: hidden;
+
+`
+
+const Block = styled.div`
+    width: 105rem;
+    height: 100%;
+    display: flex;
+
+    transition: .5s ease-in-out;
+    transform: translateX(${props => props.clickCnt ? -props.clickCnt*35 + 'rem' : 'none'});
+    z-index: 1;
+`
+
+const SliderItem = styled.div`
+    overflow: hidden;
+    width: 105rem;
+    height: 35rem;
+    background-color: ${props => props.background};
+`
+
+const Controller = styled.div`
+    background-color: black;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+
+const ControllerBtn = styled.button`
+    padding: 1.5rem 3.5rem;
+    color: white;
+    background-color: black;
+    font: inherit;
+
+`
+
+const CircleBtns = styled.div`
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
+`
+
+const CircleBtn = styled.button`
+    padding: .7rem;
+    border-radius: 50%;
+    background-color: #fff;
+`
+
 export default ({ item }) => {
 
     const { id } = useParams()
@@ -64,17 +124,32 @@ export default ({ item }) => {
         //get product data from server
     }, [id])
 
+
+    const [clickCnt, setClickCnt] = React.useState(0)
+    const [isHovered, setIsHovered] = React.useState(false)
+    
     return (
         <>
             <CardAboutWrapper>
-                <Wrapper>
-                    <Carousel>
-                    {
-                        item.imagesSrc.map((imageSrc, index) => <img key={index} src={imageSrc} alt={`product image ${index}`}/>)
-                    }
-                    </Carousel>
-                </Wrapper>
-                <Title>Blue Structube Erin Sectional Sof</Title>
+                <SliderWrapper>
+                    <Slider onMouseOver={(ev) => {setIsHovered(true)}} onMouseOut={(ev) => {setIsHovered(false)}}>
+                        <Block clickCnt={clickCnt}>
+                            <SliderItem background='yellow'/>   
+                            <SliderItem background='red'/>
+                            <SliderItem background='black'/>
+                    </Block>
+                    </Slider>
+                    <Controller>
+                        <ControllerBtn onClick={() => {setClickCnt(cnt => cnt-1)}}>Prev</ControllerBtn>
+                        <CircleBtns>
+                            <CircleBtn id='1' onClick={() => {setClickCnt(0)}}></CircleBtn>
+                            <CircleBtn id='2' onClick={() => {setClickCnt(1)}}></CircleBtn>
+                            <CircleBtn id='3' onClick={() => {setClickCnt(2)}}></CircleBtn>
+                        </CircleBtns>
+                        <ControllerBtn onClick={() => {setClickCnt(cnt => cnt+1)}}>Next</ControllerBtn>
+                    </Controller>
+                </SliderWrapper>
+                <Title>{ item.name }</Title>
                 <PriseWrapper>
                     <Prise crossedOut={ item.discountPrice && true }>{item.price+'$'}</Prise>
                     { item.discountPrice ? <DiscountPrice>{item.discountPrice+'$'}</DiscountPrice> : <></> }
