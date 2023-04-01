471a77277484407036ced864e0f04a2788a5dabc
kaisyQ <rutyo8@mail.ru>
2023-04-01 07:10:13 +0300

    sidebar-styles-fix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,13 @@
 import React from "react"
 import styled, { keyframes } from "styled-components"
 
+import * as Icon from 'react-bootstrap-icons'
+
 import { NavLink } from "react-router-dom"
 
 const openSidebar = keyframes`
     from { width: 0 }
     to { 
-        @media only screen and (max-width: 120rem){
-            width: ${props => props.isOpen ? '40rem' : '0'};
-        }
-
         @media only screen and (max-width: 76.8rem){
             width: ${props => props.isOpen ? '40rem' : '0'};
         }
@@ -41,17 +39,19 @@ const SidebarWrapper = styled.div`
     animation-duration: 0.3s;
     animation-name: ${openSidebar};
     z-index: 10;
-
-    @media only screen and (max-width: 120rem){
-        width: ${props => props.isOpen ? '40rem' : '0'};
+    
+    @media only screen and (max-width: 1200px) {
+        display: none;
     }
 
-    @media only screen and (max-width: 76.8rem){
-        width: ${props => props.isOpen ? '40rem' : '0'};
+    @media only screen and (max-width: 768px){
+        width: ${props => props.isOpen ? '60%' : '0'};
+        display: block;
     }
     
-    @media only screen and (max-width: 33rem){
-        width: ${props => props.isOpen ? '30rem' : '0'};
+    @media only screen and (max-width: 330px){
+        width: ${props => props.isOpen ? '60%' : '0'};
+        display: block;
     }
 `
 
@@ -62,13 +62,21 @@ const ListWrapper = styled.ul`
     animation-duration: 0.3s;
     animation-name: ${props => props.isOpen ? showItems : hideItems}; 
     padding: 5rem;
+    
+    @media only screen and (max-width: 330px){
+        padding: 5rem 2rem 5rem 2rem;
+    }
 `
 
 const Item = styled.li`
     padding: 1rem 1.5rem;
     font-size: 1.6rem;
+    line-height:1.6rem;
     text-transform: capitalize;
     text-align: left;
+    display: flex;
+    align-items: center;
+    column-gap: .5rem;
 
     :hover{
         color: #993255;
@@ -87,10 +95,30 @@ export default ({ isOpen, setIsOpen, isAuth, setIsDark }) => {
         <>
             <SidebarWrapper isOpen={isOpen}>
                 <ListWrapper isOpen={isOpen}>
-                    <NavLink to={'/'}><Item onClick={hideSidebar}>Home</Item></NavLink>
-                    <NavLink to={'/catalog'}><Item onClick={hideSidebar}>Catalog</Item></NavLink>
-                    <NavLink to={'/contact'}><Item onClick={hideSidebar}>Contact</Item></NavLink>
-                    <NavLink to={'/blogs'}><Item onClick={hideSidebar}>Blog</Item></NavLink>
+                    <NavLink to={'/'}>
+                        <Item onClick={hideSidebar}>
+                            <Icon.HouseFill size={'2rem'}/>
+                            <span>Home</span>
+                        </Item>
+                    </NavLink>
+                    <NavLink to={'/catalog'}>
+                        <Item onClick={hideSidebar}>
+                            <Icon.Cart size={'2rem'}/>
+                            <span>Catalog</span>
+                        </Item>
+                    </NavLink>
+                    <NavLink to={'/contact'}>
+                        <Item onClick={hideSidebar}>
+                            <Icon.EnvelopeFill size={'2rem'}/>
+                            <span>Contact</span>
+                        </Item>
+                    </NavLink>
+                    <NavLink to={'/blogs'}>
+                        <Item onClick={hideSidebar}>
+                            <Icon.Newspaper size={'2rem'}/>
+                            <span>Blog</span>
+                        </Item>
+                    </NavLink>
                         {
                             isAuth ? <>
                                     <NavLink to={'/admin/users'}><Item onClick={hideSidebar}>Users</Item></NavLink>
