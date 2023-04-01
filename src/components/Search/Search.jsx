e229573b92472450f7c59f0b98f05bf4e367e12e
kaisyQ <rutyo8@mail.ru>
2023-04-01 06:23:16 +0300

    search-styles-fix

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,21 +5,26 @@ import Input from "../Custom/Input"
 
 import * as Icon from 'react-bootstrap-icons'
 
-
-
 const SearchWrapper = styled.div`
     position: fixed;
-    width: 80%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 3rem;
+    width: 60%;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 `
 
+const SearchContainer = styled.div`
+    width: 100%;
+    position: relative;
+`
 
+const IconWrapper = styled.div`
+    position: absolute;
+    right: 3rem;
+    top: 50%;
+    transform: translateY(-50%);
+    line-height: 100%;
+`
 export default () => {
 
     const [searchVl, setSearchVl] = React.useState('')
@@ -27,8 +32,12 @@ export default () => {
     return (
         <>
             <SearchWrapper>
-                <Input fontSize={'1.8rem'} padding={'3rem 2rem'} id='header-search' placeholder='Search' value={searchVl} onChange={(ev) => {setSearchVl(ev.target.value)}}/>
-                <Icon.Search color="#c74646" size={'3.5rem'} cursor='pointer' />
+                <SearchContainer>
+                    <Input fontSize={'1.8rem'} padding={'3rem 7rem 3rem 2rem'} id='header-search' placeholder='Search' value={searchVl} onChange={(ev) => {setSearchVl(ev.target.value)}}/>
+                    <IconWrapper>
+                        <Icon.Search color="#c74646" size={'3.5rem'} cursor='pointer' />
+                    </IconWrapper>
+                </SearchContainer>
             </SearchWrapper>
         </>
     )
