32040f759a60d379dd448ff12e5c77fcad58ecaf
kaisyQ <rutyo8@mail.ru>
2023-07-08 20:40:02 +1000

    deploy-fix

--- src/api/fetch.setup/fetch.types.ts
+++ src/api/fetch.setup/fetch.types.ts
@@ -1,8 +1,5 @@
 export type HeadersType = {
     "Content-Type": string,
-    'Accept': string,
-    'Access-Control-Allow-Origin': string | string[],
-    'Access-Control-Allow-Credentials': string
 }
 
 
