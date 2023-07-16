83fbcefb75658734625d6b079b86a9d8d06d07f4
kaisyQ <rutyo8@mail.ru>
2023-07-16 19:03:46 +1000

    f

--- src/api/fetch.setup/fetch.params.ts
+++ src/api/fetch.setup/fetch.params.ts
@@ -6,7 +6,7 @@ export const withoutBodyHeaders: HeadersType = {
 }
 
 const options: HttpQueryOptionsType = {
-    baseUrl: "http://198.245.55.24/api/",
+    baseUrl: "https://198.245.55.24/api/",
     credentials: "include",
 }
 
