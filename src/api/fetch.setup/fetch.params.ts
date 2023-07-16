f1c87efb194602f8a0b2d0ba34cf5cb04b23007e
kaisyQ <rutyo8@mail.ru>
2023-07-16 18:56:54 +1000

    t

--- src/api/fetch.setup/fetch.params.ts
+++ src/api/fetch.setup/fetch.params.ts
@@ -6,7 +6,7 @@ export const withoutBodyHeaders: HeadersType = {
 }
 
 const options: HttpQueryOptionsType = {
-    baseUrl: "https://bmfurniture.ca/",
+    baseUrl: "http://bmfurniture.ca/api/",
     credentials: "include",
 }
 
