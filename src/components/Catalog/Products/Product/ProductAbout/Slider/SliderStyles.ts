556faad17d33528286d3ea4a6295ab3f21a90d20
kaisyQ <rutyo8@mail.ru>
2023-07-27 17:44:43 +1000

    fix

--- src/components/Catalog/Products/Product/ProductAbout/Slider/SliderStyles.ts
+++ src/components/Catalog/Products/Product/ProductAbout/Slider/SliderStyles.ts
@@ -9,6 +9,7 @@ export const Slider = styled.div<ISliderBlockProps>`
     max-width: ${props => props.width};
     max-height: ${props => props.width};
     overflow-x: scroll;
+    border: .1rem solid #000;
 `;
 
 export const Block = styled.div<ISliderBlockProps>`
@@ -16,7 +17,7 @@ export const Block = styled.div<ISliderBlockProps>`
     display: flex;
     z-index: 1;
     transition: .2s ease-in-out;
-
+    align-items: center;
 `;
 
 export const SliderItem = styled.img<ISliderBlockProps>`
